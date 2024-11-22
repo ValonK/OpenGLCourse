a7f2e888927c1fe5d4e4614b226046c180017912
Valon Kastrati <3otcdkwvt8x8mo2eab51iojc4exxfn@bots.bitbucket.org>
2024-11-22 14:22:25 +0100

    setup OpenGL window

--- src/OpenGLCourse/OpenGLCourse/OpenGLCourse.cpp
+++ src/OpenGLCourse/OpenGLCourse/OpenGLCourse.cpp
@@ -1,6 +1,80 @@
+#include <stdio.h>
 #include <iostream>
 
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+
+const GLint WIDTH = 800, HEIGHT = 600;
+
+using namespace std;
+
 int main()
+{
+	//Initialize GLFW 
+	if (!glfwInit())
+	{
+		cout << "GLFW Initialization failed!" << endl;
+		glfwTerminate();
+		return 1;
+	}
+
+	// Setup GLFW window properties & OpenGL version 3.3
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+
+	// dont use deprecated version / features, No backwards compat
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+	// Is forward compatible 
+	glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+
+
+	// Create Window
+	GLFWwindow* mainWindow = glfwCreateWindow(WIDTH, HEIGHT, "Test Window", NULL, NULL);
+	if (!mainWindow)
+	{
+		cout << "GLFW Window creation failed!" << endl;
+		glfwTerminate();
+		return 1;
+	}
+
+	// Get Buffer size information 
+	int bufferWidth, bufferHeight;
+	glfwGetFramebufferSize(mainWindow, &bufferWidth, &bufferHeight);
+
+	// set context for GLEW to use (also used to switch between windows)
+	glfwMakeContextCurrent(mainWindow);
+
+	// Allow modern extension features
+	glewExperimental = GL_TRUE;
+
+	if (glewInit() != GLEW_OK)
+	{
+		cout << "GLEW Initialization failed!" << endl;
+		glfwDestroyWindow(mainWindow);
+		glfwTerminate();
+		return 1;
+	}
+
+	// Setup Viewport size
+	glViewport(0, 0, bufferWidth, bufferHeight);
+
+	while (!glfwWindowShouldClose(mainWindow))
+	{
+		// Get + Handle user input events
+		glfwPollEvents();
+
+		// Clear window
+		glClearColor(1.f, 0.f, 0.f, 1.f);
+		glClear(GL_COLOR_BUFFER_BIT);
+
+		glfwSwapBuffers(mainWindow);
+	}
+
+	return 0;
+}
+
+void Initialize() 
 {
 
 }
